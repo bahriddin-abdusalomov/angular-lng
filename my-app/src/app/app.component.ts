3e52d996eadaad30d20e58109b0d7b6dbeea172b
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-14 17:12:30 +0500

    updated app.ts file

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { DialogComponent } from './dialog/dialog.component';
+import {
+  MatDialog,
+  MAT_DIALOG_DATA,
+  MatDialogTitle,
+  MatDialogContent,
+} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(private dialog: MatDialog){}
   title = 'my-app';
+  openDialog() {
+    this.dialog.open(DialogComponent, {
+      width: "30%"
+    });
+  }
 }
